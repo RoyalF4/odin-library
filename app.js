86d8435b903b3cff1852706c5284dbcd57aabb30
Royal Finch <Royal2791@gmail.com>
2024-02-02 15:24:04 -0500

    add radio button to form and fix tables 'Read' col

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const fd = new FormData(form);
     const obj = Object.fromEntries(fd);
+    console.log(obj)
     addBookToLibrary(obj);
     modal.close();
 });
